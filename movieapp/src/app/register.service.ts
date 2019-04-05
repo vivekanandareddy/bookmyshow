81c1a473441766bbcab31c9d79fec315e9190c5b
VIVEKANANDA REDDY M <vivekreddy35@gmail.com>
2019-04-05 17:00:40 +0530

    small change

--- movieapp/src/app/register.service.ts
+++ movieapp/src/app/register.service.ts
@@ -13,27 +13,7 @@ export class RegisterService {
   registerUser(user)
   {
     return this.http.post<any>(this.registerUrl,user)
-    /*let usernames=[];
-    
-    usernames.push(JSON.parse(localStorage.getItem('usernames')));
-    
-    for(let i=0;i<usernames.length;i++)
-    {
-      if(usernames[i] == user.email)
-      {
-        console.log('Username exists, Use any other Username');
-      }
-      else{
-        usernames.push(user.email);
-        
-      }
-    }
-      
-    localStorage.setItem('usernames', JSON.stringify(usernames));
-   // localStorage.setItem('email',user.email);
-    localStorage.setItem('password', user.password);
-
-    console.log(JSON.parse(localStorage.getItem('usernames'))); */
+   
     
   }
   
