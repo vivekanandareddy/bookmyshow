c3e8922251f56db898e929819766736cc24697a3
VIVEKANANDA REDDY M <vivekreddy35@gmail.com>
2019-04-05 13:28:22 +0530

    movies fetching from db

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,13 +2,18 @@ const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
+const Movies = require('../models/movies')
 
 const mongoose = require('mongoose')
 const db ="mongodb+srv://vivek:vivek@cluster1-zfbnm.mongodb.net/users?retryWrites=true"
-
+const db1 ="mongodb+srv://vivek:vivek@cluster1-zfbnm.mongodb.net/movies?retryWrites=true" 
 mongoose.connect(db, err=>{
     if(err){ console.error(err)}
-    else{ console.log('Mongodb Connected')}
+    else{ console.log('Mongodb Connected to user db')}
+})
+mongoose.connect(db1, err=>{
+    if(err){ console.error(err)}
+    else{ console.log('Mongodb Connected to movies db')}
 })
 router.get('/',(req,res)=>{
     res.send('From Api')
@@ -57,33 +62,12 @@ router.post('/login',(req,res)=>{
 })
 
 router.get('/movies',(req,res)=>{
-    let movies=[
-        {
-            "id":"1",
-            "name":"Arya",
-            "description":"Allu Arjun's First Hit Movie"
-        },
-        {
-            "id": "2",
-            "name": "Bunny",
-            "description": "Allu Arjun's Second Hit Movie"
-        },
-        {
-            "id": "3",
-            "name": "Desamuduru",
-            "description": "Allu Arjun's BlockBuster Hit Movie"
-        },
-        {
-            "id": "4",
-            "name": "Happy",
-            "description": "Allu Arjun's Sweet Hit Movie"
-        },
-        {
-            "id": "5",
-            "name": "Julai",
-            "description": "Allu Arjun's Entertaining Hit Movie"
-        }
-    ]
+    let movies=[]
+    let movie = new Movies()
+    movie.findMany((err,fetchedmovies)=>{
+        movies = fetchedmovies
+    })
+
     res.json(movies)
 })
 
